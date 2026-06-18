83852fa96c198775130a32471c548328cf66e1a7
Soyemma7w7 <emmamnuel61031@gmail.com>
2026-06-18 10:16:54 -0500

    cambio de puerto

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 45771;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server corriendo en http://localhost:${PORT}`);
