7ac5f4cd1659538668ea8a1fb1d6fb7424aada56
Soyemma7w7 <emmamnuel61031@gmail.com>
2026-06-23 10:05:03 -0500

    Cambios para envio de correo

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,14 +5,14 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    family: 4
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 exports.registro = async (req, res) => {
